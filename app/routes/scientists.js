bd555332f652345ec7726fa63643936290b2a792
Guodandan <1565451591@qq.com>
2019-07-11 12:21:17 +0800

    test

---
+++ app/routes/scientists.js
@@ -0,0 +1,13 @@
+// import Route from '@ember/routing/route';
+
+// export default Route.extend({
+// });
+
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
+});
+
